d9cd27b20e5362fd50d597ef35568009d8edc073
Yusuf <Yusuf@PRIYANDARI>
2017-03-04 21:54:07 +0700

    First

--- ProgramaKomputer2016/Program.cs
+++ ProgramaKomputer2016/Program.cs
@@ -9,6 +9,39 @@ namespace ProgramaKomputer2016
     {
         static void Main(string[] args)
         {
+            //deklarasi variabel
+            int suhuC; 
+            int suhuK;
+            string nama;
+
+            //Input Data
+            //suhuC = 100;
+            Console.Write("Inputkan nama Anda : ");
+            nama = Console.ReadLine();
+
+            Console.Write("Inputkan suhu C : ");
+            suhuC = Convert.ToInt32(Console.ReadLine());
+            //Proses
+            suhuK = 273 + suhuC;
+
+            //Output
+            //Console.Write("Suhu hasil konversi dalam Kelvin = ");
+            //Console.WriteLine(suhuK);
+
+            //Console.Write("Suhu hasil konversi dalam Kelvin = {0}", suhuK);
+
+            //Console.Write("{0} Celcius = {1} Kelvin", suhuC, suhuK);
+
+
+            Console.WriteLine("Suhu hasil konversi dalam Kelvin = " + suhuK);
+
+            Console.WriteLine("Terima kasih {0} atas perhatiannya", nama);
+            //bagian untuk menghentikan program sampai ditekannya sebuah tombol
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
